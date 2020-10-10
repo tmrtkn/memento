c25f16545cb2d17cd1b428f66bc5e0f55cbb6281
tmrtkn <toni.martikainen@gmail.com>
2020-10-11 00:06:55 +0300

    Added some file handling

--- memento_app.py
+++ memento_app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 from flask_httpauth import HTTPTokenAuth
+from datetime import datetime
+
+NEW_URL_FILE = 'newUrl.txt'
 
 app = Flask(__name__)
 auth = HTTPTokenAuth('Memento')
@@ -15,7 +18,6 @@ def verify_token(token):
     if token in tokens:
         return tokens[token]
 
-
 @app.route('/')
 @auth.login_required
 def index():
@@ -26,6 +28,7 @@ def index():
 def add():
     try:
         url = request.form['url']
+        appendUrl(url)
     except:
         print('No Url given')
 
@@ -40,7 +43,12 @@ def add():
 
     return "200 OK"
 
+def appendUrl(url):
+    print("Opening " + NEW_URL_FILE)
+    f = open(NEW_URL_FILE, "a+")
+    f.write(auth.current_user() + ":\t" + str(datetime.today()) + ":\t" + url + '\n')
+    print("Writing " + url + " to " + NEW_URL_FILE)
+    f.close()
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc', host='0.0.0.0')
-    # app.run(ssl_context='adhoc', host='0.0.0.0')
